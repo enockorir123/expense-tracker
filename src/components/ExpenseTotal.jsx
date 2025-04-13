1b09383949490207e2c35bef16d5e680df585e8d
Enock Cheruiyot Korir <enockorir123@gmai.com>
2025-04-13 20:02:07 +0300

    add expense total component

---
+++ src/components/ExpenseTotal.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function ExpenseTotal({ expenses }) {
+  const total = expenses.reduce((sum, expense) => sum + expense.amount, 0);
+
+  return (
+    <div style={{ marginTop: '1.5rem' }}>
+      <h2>Total Expenses: ${total.toFixed(2)}</h2>
+    </div>
+  );
+}
+
+export default ExpenseTotal;
